9ed33428391df4bfc074704e433a3d5a1adf782d
Rodrigo Ramirez <rodrigoramirez@librodepases.com>
2025-02-19 03:13:04 -0300

    agregar link de wpp

--- components/ContactModal/ContactModal.tsx
+++ components/ContactModal/ContactModal.tsx
@@ -1,7 +1,11 @@
-import { Button, Flex, Modal, QRCode } from "antd";
+import { Flex, Modal, QRCode } from "antd";
 import React from "react";
 import Title from "../ui/Title";
 import Text from "../ui/Text";
+import SLogo from "@/public/assets/s-suash-logo.png";
+import { ModalButton } from "./style";
+import Link from "next/link";
+import { FaWhatsapp } from "react-icons/fa6";
 
 type ContactModalProps = {
     modalConfig: {
@@ -14,23 +18,31 @@ type ContactModalProps = {
 const ContactModal = ({ modalConfig }: ContactModalProps) => {
     return (
         <Modal
-            title="Contacto"
             open={modalConfig.isModalOpen}
             onCancel={modalConfig.handleCancel}
             footer={[
-                <Button key="back" onClick={modalConfig.handleCancel}>
-                    Cerrar
-                </Button>,
-
-                <Button
+                <ModalButton
                     key="link"
-                    href="/"
-                    target="_blank"
-                    type="primary"
                     onClick={modalConfig.handleOk}
+                    className="text-black hover:text-white hover:bg-black dark:text-white dark:hover:text-black dark:hover:bg-white"
                 >
-                    Ir a whatsapp
-                </Button>,
+                    Cerrar
+                </ModalButton>,
+
+                <ModalButton
+                    key="link"
+                    onClick={modalConfig.handleOk}
+                    className="text-black hover:text-white hover:bg-black dark:text-white dark:hover:text-black dark:hover:bg-white"
+                >
+                    <Link
+                        href="https://wa.me/+5492266679724"
+                        target="_blank"
+                        className="hover:text-white dark:hover:text-black flex items-center gap-2"
+                    >
+                        <span>Ir a whatsapp</span>
+                        <FaWhatsapp />
+                    </Link>
+                </ModalButton>,
             ]}
         >
             <Flex
@@ -40,7 +52,7 @@ const ContactModal = ({ modalConfig }: ContactModalProps) => {
                 gap="large"
                 style={{ width: "100%" }}
             >
-                <Title level={4} fontSize="24px" fontWeight="700">
+                <Title level={4} fontSize="24px" fontWeight="400">
                     Contacto
                 </Title>
                 <Flex
@@ -50,18 +62,21 @@ const ContactModal = ({ modalConfig }: ContactModalProps) => {
                     gap="large"
                     style={{ width: "100%" }}
                 >
-                    <QRCode
-                        type="svg"
-                        value="https://ant.design/"
-                        icon="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
-                    />
                     <Text
                         fontSize="16px"
                         fontWeight="400"
-                        style={{ marginBottom: "20px" }}
+                        style={{ margin: "10px 0px" }}
                     >
-                        Ir al link o escanear el QA
+                        Ir al link o escanear el QR
                     </Text>
+                    <QRCode
+                        type="svg"
+                        value="https://wa.me/+5492266679724"
+                        icon={SLogo.src}
+                        size={300}
+                        iconSize={80}
+                        style={{ marginBottom: "30px" }}
+                    />
                 </Flex>
             </Flex>
         </Modal>
---
+++ components/ContactModal/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ModalButton = styled.button`
+    border: 1px solid #000;
+    box-sizing: border-box;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: 0.3s ease-in-out;
+    font-family: "kanit", sans-serif;
+    font-size: 1rem;
+    font-weight: 500;
+    margin-left: 10px;
+`;
